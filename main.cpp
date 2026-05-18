1e6877608b8cae30f886e2690fa92ef11c5c2dd3
Developer <dev@example.com>
2026-05-19 00:20:30 +0800

    添加窗口拖拽调整大小功能
    - 检测鼠标在窗口边缘位置
    - 支持八个方向调整大小
    - 动态更新鼠标光标样式
    - 保存和恢复窗口大小
    - 添加 .gitignore 忽略 build 目录

--- main.cpp
+++ main.cpp
@@ -1,153 +1,231 @@
 #include <DApplication>
-#include <DMainWindow>
+#include <DPlatformWindowHandle>
 #include "todowidget.h"
 
+#include <QMainWindow>
 #include <QScreen>
 #include <QSettings>
 #include <QGuiApplication>
 #include <QMouseEvent>
 #include <QCloseEvent>
-#include <QWindow>
-
-#include <X11/Xlib.h>
-#include <X11/Xatom.h>
 
 DWIDGET_USE_NAMESPACE
 
-class DesktopWidget : public DMainWindow
+class DesktopWidget : public QMainWindow
 {
     Q_OBJECT
 
 public:
     explicit DesktopWidget(QWidget *parent = nullptr)
-        : DMainWindow(parent)
+        : QMainWindow(parent)
         , m_dragging(false)
+        , m_resizing(false)
+        , m_resizeEdge(EdgeNone)
+        , m_handle(nullptr)
     {
-        // 无边框 + 保持在底部
+        // 无边框、保持在底部
         setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnBottomHint);
         
         // 窗口透明
         setAttribute(Qt::WA_TranslucentBackground);
         setAttribute(Qt::WA_ShowWithoutActivating);
         
-        // 设置窗口属性
-        setWindowRadius(12);
-        setBorderWidth(1);
-        setBorderColor(QColor(255, 255, 255, 25));
-        setShadowRadius(30);
-        setShadowOffset(QPoint(0, 10));
-        setShadowColor(QColor(0, 0, 0, 80));
-        setTranslucentBackground(true);
-        setEnableSystemResize(false);
-        setEnableSystemMove(false);
-        setEnableBlurWindow(true);
+        // 启用 dxcb 平台处理
+        DPlatformWindowHandle::enableDXcbForWindow(this);
+        m_handle = new DPlatformWindowHandle(this, this);
+        
+        // 设置窗口圆角
+        m_handle->setWindowRadius(12);
+        
+        // 设置边框
+        m_handle->setBorderWidth(1);
+        m_handle->setBorderColor(QColor(80, 80, 80, 180));
+        
+        // 设置阴影
+        m_handle->setShadowRadius(20);
+        m_handle->setShadowOffset(QPoint(0, 8));
+        m_handle->setShadowColor(QColor(0, 0, 0, 100));
+        
+        // 透明背景
+        m_handle->setTranslucentBackground(true);
+        m_handle->setEnableSystemResize(false);
+        m_handle->setEnableSystemMove(false);
+        m_handle->setEnableBlurWindow(true);
         
         // 创建 TodoWidget
         m_todoWidget = new TodoWidget(this);
         setCentralWidget(m_todoWidget);
         
-        setFixedSize(350, 500);
-        loadPosition();
+        // 设置最小和初始大小
+        setMinimumSize(250, 300);
+        resize(350, 500);
+        
+        // 加载保存的位置和大小
+        loadGeometry();
     }
 
 protected:
-    void showEvent(QShowEvent *event) override
-    {
-        DMainWindow::showEvent(event);
-        // 窗口显示后设置 SkipTaskbar/SkipPager
-        setSkipTaskbar();
-    }
-
     void mousePressEvent(QMouseEvent *event) override
     {
         if (event->button() == Qt::LeftButton) {
-            m_dragging = true;
-            m_dragPos = event->globalPosition().toPoint() - frameGeometry().topLeft();
+            m_resizeEdge = getResizeEdge(event->pos());
+            if (m_resizeEdge != EdgeNone) {
+                m_resizing = true;
+                m_resizeStartPos = event->globalPosition().toPoint();
+                m_resizeStartGeom = geometry();
+            } else {
+                m_dragging = true;
+                m_dragPos = event->globalPosition().toPoint() - frameGeometry().topLeft();
+            }
             event->accept();
         }
-        DMainWindow::mousePressEvent(event);
+        QMainWindow::mousePressEvent(event);
     }
 
     void mouseMoveEvent(QMouseEvent *event) override
     {
-        if (m_dragging) {
+        if (m_resizing) {
+            QPoint delta = event->globalPosition().toPoint() - m_resizeStartPos;
+            QRect newGeom = m_resizeStartGeom;
+            
+            int minWidth = minimumWidth();
+            int minHeight = minimumHeight();
+            
+            if (m_resizeEdge & EdgeLeft) {
+                newGeom.setLeft(m_resizeStartGeom.left() + delta.x());
+                if (newGeom.width() < minWidth) {
+                    newGeom.setLeft(m_resizeStartGeom.right() - minWidth);
+                }
+            }
+            if (m_resizeEdge & EdgeRight) {
+                newGeom.setRight(m_resizeStartGeom.right() + delta.x());
+                if (newGeom.width() < minWidth) {
+                    newGeom.setRight(m_resizeStartGeom.left() + minWidth);
+                }
+            }
+            if (m_resizeEdge & EdgeTop) {
+                newGeom.setTop(m_resizeStartGeom.top() + delta.y());
+                if (newGeom.height() < minHeight) {
+                    newGeom.setTop(m_resizeStartGeom.bottom() - minHeight);
+                }
+            }
+            if (m_resizeEdge & EdgeBottom) {
+                newGeom.setBottom(m_resizeStartGeom.bottom() + delta.y());
+                if (newGeom.height() < minHeight) {
+                    newGeom.setBottom(m_resizeStartGeom.top() + minHeight);
+                }
+            }
+            
+            setGeometry(newGeom);
+            event->accept();
+        } else if (m_dragging) {
             move(event->globalPosition().toPoint() - m_dragPos);
             event->accept();
+        } else {
+            // 更新鼠标光标
+            Edge edge = getResizeEdge(event->pos());
+            updateCursor(edge);
         }
-        DMainWindow::mouseMoveEvent(event);
+        QMainWindow::mouseMoveEvent(event);
     }
 
     void mouseReleaseEvent(QMouseEvent *event) override
     {
         if (event->button() == Qt::LeftButton) {
-            m_dragging = false;
-            savePosition();
+            if (m_resizing) {
+                m_resizing = false;
+                saveGeometry();
+            } else if (m_dragging) {
+                m_dragging = false;
+                saveGeometry();
+            }
             event->accept();
+            setCursor(Qt::ArrowCursor);
         }
-        DMainWindow::mouseReleaseEvent(event);
+        QMainWindow::mouseReleaseEvent(event);
     }
 
     void closeEvent(QCloseEvent *event) override
     {
-        savePosition();
-        DMainWindow::closeEvent(event);
+        saveGeometry();
+        QMainWindow::closeEvent(event);
     }
 
 private:
-    void setSkipTaskbar()
+    enum Edge {
+        EdgeNone = 0,
+        EdgeLeft = 1,
+        EdgeRight = 2,
+        EdgeTop = 4,
+        EdgeBottom = 8
+    };
+
+    Edge getResizeEdge(const QPoint &pos)
     {
-        Display *display = XOpenDisplay(nullptr);
-        if (!display) return;
+        const int margin = 8;
+        Edge edge = EdgeNone;
         
-        Window window = static_cast<Window>(winId());
+        if (pos.x() < margin) edge = Edge(edge | EdgeLeft);
+        else if (pos.x() > width() - margin) edge = Edge(edge | EdgeRight);
         
-        Atom stateAtom = XInternAtom(display, "_NET_WM_STATE", False);
-        Atom skipTaskbar = XInternAtom(display, "_NET_WM_STATE_SKIP_TASKBAR", False);
-        Atom skipPager = XInternAtom(display, "_NET_WM_STATE_SKIP_PAGER", False);
+        if (pos.y() < margin) edge = Edge(edge | EdgeTop);
+        else if (pos.y() > height() - margin) edge = Edge(edge | EdgeBottom);
         
-        XEvent event = {};
-        event.xclient.type = ClientMessage;
-        event.xclient.window = window;
-        event.xclient.message_type = stateAtom;
-        event.xclient.format = 32;
-        event.xclient.data.l[0] = 1; // _NET_WM_STATE_ADD
-        event.xclient.data.l[1] = skipTaskbar;
-        event.xclient.data.l[2] = skipPager;
-        event.xclient.data.l[3] = 0;
-        event.xclient.data.l[4] = 0;
-        
-        XSendEvent(display, DefaultRootWindow(display), False,
-                   SubstructureRedirectMask | SubstructureNotifyMask, &event);
-        XFlush(display);
-        XCloseDisplay(display);
+        return edge;
     }
 
-    void savePosition()
+    void updateCursor(Edge edge)
     {
-        QSettings settings("dtk-todo-widget", "position");
+        if (edge == EdgeNone) {
+            setCursor(Qt::ArrowCursor);
+        } else if (edge == EdgeLeft || edge == EdgeRight) {
+            setCursor(Qt::SizeHorCursor);
+        } else if (edge == EdgeTop || edge == EdgeBottom) {
+            setCursor(Qt::SizeVerCursor);
+        } else if (edge == (EdgeLeft | EdgeTop) || edge == (EdgeRight | EdgeBottom)) {
+            setCursor(Qt::SizeFDiagCursor);
+        } else {
+            setCursor(Qt::SizeBDiagCursor);
+        }
+    }
+
+    void saveGeometry()
+    {
+        QSettings settings("dtk-todo-widget", "geometry");
         settings.setValue("pos", pos());
+        settings.setValue("size", size());
     }
 
-    void loadPosition()
+    void loadGeometry()
     {
-        QSettings settings("dtk-todo-widget", "position");
+        QSettings settings("dtk-todo-widget", "geometry");
         QPoint savedPos = settings.value("pos").toPoint();
+        QSize savedSize = settings.value("size").toSize();
         
         QScreen *screen = QGuiApplication::primaryScreen();
         QRect screenRect = screen->availableGeometry();
+        
+        if (savedSize.isValid() && savedSize.width() >= minimumWidth() && savedSize.height() >= minimumHeight()) {
+            resize(savedSize);
+        }
         
         if (savedPos.x() >= 0 && savedPos.y() >= 0 &&
             savedPos.x() < screenRect.width() - 100 &&
             savedPos.y() < screenRect.height() - 100) {
             move(savedPos);
         } else {
-            move(screenRect.right() - 380, screenRect.bottom() - 550);
+            move(screenRect.right() - width() - 30, screenRect.bottom() - height() - 50);
         }
     }
 
     TodoWidget *m_todoWidget;
+    DPlatformWindowHandle *m_handle;
     bool m_dragging;
+    bool m_resizing;
     QPoint m_dragPos;
+    QPoint m_resizeStartPos;
+    QRect m_resizeStartGeom;
+    int m_resizeEdge;
 };
 
 int main(int argc, char *argv[])
