fab6f571f2eecbabeaea03de33786e27c359b7b4
Developer <dev@example.com>
2026-05-19 00:18:54 +0800

    添加 .gitignore 忽略 build 目录

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_dtk-todo-widget_FILE /home/uos/widgets/dtk-todo-widget/build/dtk-todo-widget)
-set(__QT_DEPLOY_TARGET_dtk-todo-widget_TYPE EXECUTABLE)
--- build/CMakeFiles/dtk-todo-widget_autogen.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for dtk-todo-widget_autogen.
